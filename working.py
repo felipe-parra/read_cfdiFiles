b129ec950b1e7ea1c626965b1fb8500a256f4cfc
pipepico <fpr29.10@gmail.com>
2020-01-26 14:20:15 -0600

    update working.py

--- working.py
+++ working.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 # -*- coding:utf-8 -*-
-
+# -*- coding: utf-8 -*-
 
 import time
+
+from datetime import datetime
 
 import os
 
@@ -13,14 +15,28 @@ import sqlite3
 
 import glob
 
-# import pandas as pd
+import shutil
 
 from xml.dom import minidom
 
 
 
-def open_file(file_name):
+class Producto:
+    def __init__(self,clave,descripcion,precio):
+        self.clave = clave
+        self.descripcion = descripcion
+        self.precio = precio
+        
+    def myfunc(self):
+        return self.clave,self.descripcion,self.precio
+        # print("{} - {} - ${}".format(self.clave,self.descripcion,self.precio))
+        # print(self.clave,self.descripcion,"$"+self.precio)
 
+    def printdata(self):
+        self.myfunc()
+
+
+def open_file(file_name):
     """Funcion que extrae informacion de un cfdi, y la devuelve en un diccionario"""
 
     global datos_cfdi
@@ -49,25 +65,29 @@ def open_file(file_name):
 
     global uuid_relacionado
 
+    global cAt
+
+    global uAt
+
     try:
 
         doc = minidom.parse(file_name)
 
     except:
 
-         return
-
-
+        return
 
     doc = minidom.parse(file_name)
 
     uuid_relacionado = ''
 
     datos_cfdi = doc.getElementsByTagName("cfdi:Comprobante")
-
+    
     for dato_cfdi in datos_cfdi:
 
         get_version = dato_cfdi.getAttribute("Version")
+        cAt = datetime.now().isoformat()
+        uAt = datetime.now().isoformat()
 
         if(get_version):
 
@@ -89,13 +109,13 @@ def open_file(file_name):
 
                 tipo_doc = "INGRESO"
 
-                rfc_emisor      = get_rfc(dato_cfdi,"Emisor")
+                rfc_emisor = get_rfc(dato_cfdi, "Emisor")
 
-                nombre_emisor   = get_nombre(dato_cfdi,"Emisor")
+                nombre_emisor = get_nombre(dato_cfdi, "Emisor")
 
-                rfc_receptor    = get_rfc(dato_cfdi,"Receptor")
+                rfc_receptor = get_rfc(dato_cfdi, "Receptor")
 
-                nombre_receptor = get_nombre(dato_cfdi,"Receptor")
+                nombre_receptor = get_nombre(dato_cfdi, "Receptor")
 
             elif(tipo_doc == "E"):
 
@@ -103,13 +123,13 @@ def open_file(file_name):
 
                 uuid_relacionado = iteracion_relacion_cfdi(doc)
 
-                rfc_emisor      = get_rfc(dato_cfdi,"Emisor")
+                rfc_emisor = get_rfc(dato_cfdi, "Emisor")
 
-                nombre_emisor   = get_nombre(dato_cfdi,"Emisor")
+                nombre_emisor = get_nombre(dato_cfdi, "Emisor")
 
-                rfc_receptor    = get_rfc(dato_cfdi,"Receptor")
+                rfc_receptor = get_rfc(dato_cfdi, "Receptor")
 
-                nombre_receptor = get_nombre(dato_cfdi,"Receptor")
+                nombre_receptor = get_nombre(dato_cfdi, "Receptor")
 
             elif(tipo_doc == "P"):
 
@@ -117,39 +137,39 @@ def open_file(file_name):
 
                 uuid_relacionado = iteracion_relacion_cfdi(doc)
 
-                rfc_emisor      = get_rfc(dato_cfdi,"Emisor")
+                rfc_emisor = get_rfc(dato_cfdi, "Emisor")
 
-                nombre_emisor   = get_nombre(dato_cfdi,"Emisor")
+                nombre_emisor = get_nombre(dato_cfdi, "Emisor")
 
-                rfc_receptor    = get_rfc(dato_cfdi,"Receptor")
+                rfc_receptor = get_rfc(dato_cfdi, "Receptor")
 
-                nombre_receptor = get_nombre(dato_cfdi,"Receptor")
+                nombre_receptor = get_nombre(dato_cfdi, "Receptor")
 
         else:
 
-            tipo_doc            = ""
+            tipo_doc = ""
 
-            serie               = ""
+            serie = ""
 
-            folio               = ""
+            folio = ""
 
-            folio_factura       = ""
+            folio_factura = ""
 
-            total_factura       = ""
+            total_factura = ""
 
-            str_total           = ""
+            str_total = ""
 
-            rfc_emisor          = ""
+            rfc_emisor = ""
 
-            rfc_receptor        = ""
+            rfc_receptor = ""
 
-            uuid                = ""
+            uuid = ""
 
-            f_exp               = ""
+            f_exp = ""
 
-            f_tim               = ""
+            f_tim = ""
 
-            uuid_relacionado    = ""
+            uuid_relacionado = ""
 
             return False
 
@@ -161,29 +181,29 @@ def open_file(file_name):
 
         for datoTimbrado in datos_timbrado:
 
-            uuid    = datoTimbrado.getAttribute("UUID")
+            uuid = datoTimbrado.getAttribute("UUID")
 
-            f_tim   = datoTimbrado.getAttribute("FechaTimbrado")
+            f_tim = datoTimbrado.getAttribute("FechaTimbrado")
 
     if (tipo_doc == "INGRESO"):
 
-        guardando_en_bd_FACT(folio_factura,total_factura,f_exp,f_tim,uuid,rfc_emisor,rfc_receptor)
+        guardando_en_bd_FACT(folio_factura, total_factura, f_exp,
+                             f_tim, uuid, rfc_emisor, rfc_receptor, cAt, uAt)
 
     elif (tipo_doc == "EGRESO"):
 
-            guardando_en_bd_NC(folio_factura,total_factura,f_exp,f_tim,uuid,uuid_relacionado,rfc_emisor,rfc_receptor)
+        guardando_en_bd_NC(folio_factura, total_factura, f_exp, f_tim,
+                           uuid, uuid_relacionado, rfc_emisor, rfc_receptor, cAt, uAt)
 
     """elif (tipo_doc = "PAGO"):
 
         guardando_en_bd_PAGO(folio_factura,str_total,f_exp,uuid,rfc_emisor,rfc_receptor)"""
 
-    print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(folio_factura,total_factura,f_exp,uuid,rfc_emisor,rfc_receptor,uuid_relacionado))
+    print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(folio_factura, total_factura,
+                                              f_exp, uuid, rfc_emisor, rfc_receptor, uuid_relacionado))
 
 
-
-
-
-def get_rfc(doc,tipo):
+def get_rfc(doc, tipo):
 
     datos = doc.getElementsByTagName("cfdi:{}".format(tipo))
 
@@ -194,10 +214,7 @@ def get_rfc(doc,tipo):
     return rfc
 
 
-
-
-
-def get_nombre(doc,tipo):
+def get_nombre(doc, tipo):
 
     datos = doc.getElementsByTagName("cfdi:{}".format(tipo))
 
@@ -206,9 +223,6 @@ def get_nombre(doc,tipo):
         nombre = dato.getAttribute("Nombre")
 
     return nombre
-
-
-
 
 
 def iteracion_relacion_cfdi(doc):
@@ -227,8 +241,8 @@ def iteracion_relacion_cfdi(doc):
 
     return u_relacion
 
-
-
+def iteracion_por_producto(doc):
+    pass
 
 
 def consulta(consultar):
@@ -236,6 +250,7 @@ def consulta(consultar):
     valor = input("Ingrese {}:\t".format(consultar))
 
     return valor
+
 
 """
 
@@ -254,56 +269,67 @@ rfc_e = consulta('rfc_e')
 """
 
 
-
-def guardando_en_bd_FACT(folio,importe,f_exp,f_tim,uuid,rfc_e,rfc_r):
+def guardando_en_bd_FACT(folio, importe, f_exp, f_tim, uuid, rfc_e, rfc_r, cAt, uAt):
 
     conn = sqlite3.connect('cfdis.db')
 
     c = conn.cursor()
 
-    c.execute("INSERT OR IGNORE INTO facturas(folio,importe,Fecha_expedicion,fecha_timbrado,UUID,id_proveedor,id_empresa) VALUES(?,?,?,?,?,?,?)",(folio,importe,f_exp,f_tim,uuid,rfc_e,rfc_r))
+    c.execute("INSERT OR IGNORE INTO facturas(folio,importe,Fecha_expedicion,fecha_timbrado,UUID,id_proveedor,id_empresa,createdAt,updatedAt) VALUES(?,?,?,?,?,?,?,?,?)",
+              (folio, importe, f_exp, f_tim, uuid, rfc_e, rfc_r, cAt, uAt))
 
     conn.commit()
 
     conn.close()
 
 
-
-
-
-def guardando_en_bd_NC(folio,importe,f_exp,f_tim,uuid,uuid_relacionado,rfc_e,rfc_r):
+def guardando_en_bd_NC(folio, importe, f_exp, f_tim, uuid, uuid_relacionado, rfc_e, rfc_r, cAt, uAt):
 
     conn = sqlite3.connect('cfdis.db')
 
     c = conn.cursor()
 
-    c.execute("INSERT OR IGNORE INTO notas_credito(folio,importe,fecha_expedicion,fecha_timbrado,UUID,UUID_RELACIONADO,id_proveedor,id_empresa) VALUES(?,?,?,?,?,?,?,?)",(folio,importe,f_exp,f_tim,uuid,uuid_relacionado,rfc_e,rfc_r))
+    c.execute("INSERT OR IGNORE INTO notas_creditos(folio,importe,fecha_expedicion,fecha_timbrado,UUID,UUID_RELACIONADO,id_proveedor,id_empresa,createdAt,updatedAt) VALUES(?,?,?,?,?,?,?,?,?,?)",
+              (folio, importe, f_exp, f_tim, uuid, uuid_relacionado, rfc_e, rfc_r, cAt, uAt))
 
     conn.commit()
 
     conn.close()
-
 
 
 if __name__ == '__main__':
-    directorio = "Downloads/"
-    directory = directorio
-    retract = "../"
-    # archivos = glob.glob("{}*.xml".format("../../../../Downloads/"))
-    while(os.path.exists(directory) != True):
-        directory = retract + directory
-    
-    archivos = glob.glob("{}*.xml".format(directory))
+
+    archivos = glob.glob("{}*.xml".format("../Downloads/"))
+
     contador = 0
 
+    # open_file("../Downloads/xmls/SO1_XM_CFDI_FacturaNormal_CF_242550_242550.xml")
     for i in archivos:
 
         contador += 1
-
         open_file(i)
+        
+        shutil.move(i, '../Downloads/xmls/')
 
-    print("Numero de Archivos consultados:\t", contador)
+    print('Numero de Archivos consultados:\t{}'.format(contador))
 
-    #guardando_en_bd_FACT(folio,importe,f_exp,uuid,rfc)
+    # guardando_en_bd_FACT(folio,importe,f_exp,uuid,rfc)
 
 
+
+"""
+    # iteracion para productos
+    datos_conceptos = doc.getElementsByTagName("cfdi:Conceptos")
+    for dato_concepto in datos_conceptos:
+        datos_concepto = dato_concepto.getElementsByTagName("cfdi:Concepto")
+        
+        for conceptos in datos_concepto:
+            # product = Producto(conceptos.getAttribute("ClaveProdServ"),conceptos.getAttribute("Descripcion"),conceptos.getAttribute("ValorUnitario"))
+            atributos = conceptos.attributes
+
+            # print(dir(atributos))
+            print(atributos.keys)
+            # print(product.myfunc())
+            # product.printdata()
+            # print(conceptos.getAttribute("Descripcion") +" - $" + conceptos.getAttribute("ValorUnitario"))
+   """
